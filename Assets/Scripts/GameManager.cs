5a105e31eff1ac8f40750d75905e9782915591e8
Yozdel-Alanis <yox009@gmail.com>
2020-08-05 17:39:52 -0500

    Animación del botón y programada la barra de tiempo y puntaje

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Animator scoreLabelAnimation;
+
     public Image timeBar;
     public float totalTime = 10;
 
@@ -18,9 +22,17 @@ public class GameManager : MonoBehaviour
 
     private int currentQuestionIndex;
 
+    private int currentScore;
+
+    public bool isGameActive;
+    public float timer;
+
+
     private void Start()
     {
+        isGameActive = true;
         scoreLabel.text = "0";
+        RestartTimer();
 
         questionLabel.text = questions[0].question;
 
@@ -33,24 +45,60 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        timeBar.fillAmount -= Time.deltaTime / totalTime;
+        if (!isGameActive)
+            return;
+
+        timer -= Time.deltaTime;
+
+        timeBar.fillAmount = timer / totalTime;
+
+        if(timer < 0)
+        {
+            NextQuestion();
+        }
     }
 
-    public void NextQuestion()
+    private void NextQuestion()
     {
         currentQuestionIndex++;
 
-        questionLabel.text = questions[currentQuestionIndex].question;
-
-        for (int i = 0; i < questions[currentQuestionIndex].options.Length; i++)
+        if(currentQuestionIndex < questions.Length)
         {
-            optionLabel[i].text = questions[currentQuestionIndex].options[i].option;
+            timeBar.fillAmount = 1.0f;
+            questionLabel.text = questions[currentQuestionIndex].question;
+
+            for (int i = 0; i < questions[currentQuestionIndex].options.Length; i++)
+            {
+                optionLabel[i].text = questions[currentQuestionIndex].options[i].option;
+            }
+        }
+        else
+        {
+            Debug.Log("Terminamos");
         }
     }
 
     public void OptionSelected(int index)
     {
-        Debug.Log("Button " + index + "was selected");
+        if (questions[currentQuestionIndex].options[index].isCorrect)
+        {
+            currentScore += 10;
+            scoreLabel.text = currentScore.ToString();
+            scoreLabelAnimation.SetTrigger("ScoreAnimation");
+        }
+        else
+        {
+            currentScore -= 10;
+            scoreLabel.text = currentScore.ToString();
+        }
+
+        NextQuestion();
+    }
+
+    private void RestartTimer()
+    {
+        timer = totalTime;
+        timeBar.fillAmount = 1.0f;
     }
 }
 
